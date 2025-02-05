e2c2e6eb5d785f10d5ce3b2804b9c2516145c7ca
Naga Hema Chand Chinta <42846711+Nagahemachand@users.noreply.github.com>
2025-02-05 17:31:54 -0600

    Adaptability on various devices

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,51 @@ import os
 import dotenv
 import uuid
 
-# Ensure Streamlit adapts to all devices
-st.set_page_config(
-    page_title="RAG LLM App",
-    page_icon="📚",
-    layout="wide",  # Makes it responsive on all screen sizes
-    initial_sidebar_state="expanded",
+# check if it's linux so it works on Streamlit Cloud
+if os.name == 'posix':
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+from langchain_openai import ChatOpenAI, AzureChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain.schema import HumanMessage, AIMessage
+
+from rag_methods import (
+    load_doc_to_db, 
+    load_url_to_db,
+    stream_llm_response,
+    stream_llm_rag_response,
 )
 
-# Apply CSS to remove sidebar padding and adjust heading sizes
-st.markdown("""
-    <style>
-        /* Move sidebar content to the top */
-        [data-testid="stSidebarContent"] {
-            padding-top: 0px !important;
-            margin-top: -40px !important; /* Adjust this value if needed */
-        }
-        /* Reduce default padding in sidebar */
-        section[data-testid="stSidebar"] > div:first-child {
-            padding-top: 0px !important;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# Load environment variables
 dotenv.load_dotenv()
 
-# Header with responsive styling
-st.markdown("<h1 style='text-align: center; font-size: 24px;'>🔍 <i>RAG Chatbot</i></h1>", unsafe_allow_html=True)
+if "AZ_OPENAI_API_KEY" not in os.environ:
+    MODELS = [
+        # "openai/o1-mini",
+        "openai/gpt-3.5-turbo",
+        "openai/gpt-4",
+        "openai/gpt-4o",
+        "openai/gpt-4o-mini",
+        "anthropic/claude-3-5-sonnet-20240620",
+    ]
+else:
+    MODELS = ["azure-openai/gpt-4o"]
 
+
+# st.set_page_config(
+#     page_title="RAG LLM app?", 
+#     page_icon="📚", 
+#     layout="centered", 
+#     initial_sidebar_state="expanded"
+# )
+
+
+# --- Header ---
+st.html("""<h1 style="text-align: center;">🔍 <i> RAG Chatbot </i> </h1>""")
+
+
+# --- Initial Setup ---
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
 
@@ -42,64 +58,131 @@ if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "user", "content": "Hello"},
         {"role": "assistant", "content": "Hi there! How can I assist you today?"}
-    ]
+]
 
-# Define available models
-if "AZ_OPENAI_API_KEY" not in os.environ:
-    MODELS = [
-        "openai/gpt-3.5-turbo",
-        "openai/gpt-4",
-        "openai/gpt-4o",
-        "openai/gpt-4o-mini",
-        "anthropic/claude-3-5-sonnet-20240620",
-    ]
-else:
-    MODELS = ["azure-openai/gpt-4o"]
 
-# Sidebar & Main Content layout for responsiveness
-sidebar, main_content = st.columns([1, 3])
-
-with sidebar:
-    st.markdown("<h2 style='text-align: left; font-size: 24px;'>🔐 API Keys</h2>", unsafe_allow_html=True)
+# --- Side Bar LLM API Tokens ---
+with st.sidebar:
     if "AZ_OPENAI_API_KEY" not in os.environ:
-        openai_api_key = st.text_input("Enter OpenAI API Key", type="password")
-        anthropic_api_key = st.text_input("Enter Anthropic API Key", type="password")
+        default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
+        with st.popover("🔐 OpenAI"):
+            openai_api_key = st.text_input(
+                "Introduce your OpenAI API Key (https://platform.openai.com/)", 
+                value=default_openai_api_key, 
+                type="password",
+                key="openai_api_key",
+            )
+
+        default_anthropic_api_key = os.getenv("ANTHROPIC_API_KEY") if os.getenv("ANTHROPIC_API_KEY") is not None else ""
+        with st.popover("🔐 Anthropic"):
+            anthropic_api_key = st.text_input(
+                "Introduce your Anthropic API Key (https://console.anthropic.com/)", 
+                value=default_anthropic_api_key, 
+                type="password",
+                key="anthropic_api_key",
+            )
     else:
         openai_api_key, anthropic_api_key = None, None
         st.session_state.openai_api_key = None
         az_openai_api_key = os.getenv("AZ_OPENAI_API_KEY")
         st.session_state.az_openai_api_key = az_openai_api_key
 
-    st.markdown("<h2 style='text-align: left; font-size: 24px;'>🤖 Model Selection</h2>", unsafe_allow_html=True)
-    st.selectbox("Choose a Model", MODELS, key="model")
 
-    # with st.expander("⚙️ Use RAG Mode", expanded=True):
-    #     is_vector_db_loaded = "vector_db" in st.session_state and st.session_state.vector_db is not None
-    #     st.toggle("Enable RAG", value=is_vector_db_loaded, key="use_rag", disabled=not is_vector_db_loaded)
+# --- Main Content ---
+# Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
+missing_openai = openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key
+missing_anthropic = anthropic_api_key == "" or anthropic_api_key is None
+if missing_openai and missing_anthropic and ("AZ_OPENAI_API_KEY" not in os.environ):
+    st.write("#")
+    st.warning("⬅️ Please choose a model of the left and enter your API Key to continue...")
 
-    st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
+else:
+    # Sidebar
+    with st.sidebar:
+        st.divider()
+        models = []
+        for model in MODELS:
+            if "openai" in model and not missing_openai:
+                models.append(model)
+            elif "anthropic" in model and not missing_anthropic:
+                models.append(model)
+            elif "azure-openai" in model:
+                models.append(model)
 
-    with st.expander("📄 Upload RAG Documents", expanded=True):
-        is_vector_db_loaded = "vector_db" in st.session_state and st.session_state.vector_db is not None
-        st.toggle("Enable RAG", value=is_vector_db_loaded, key="use_rag", disabled=not is_vector_db_loaded)
-        st.file_uploader("Upload a document", type=["pdf", "txt", "docx", "md"], accept_multiple_files=True, key="rag_docs")
+        st.selectbox(
+            "🤖 Select a Model", 
+            options=models,
+            key="model",
+        )
 
-    with st.expander("🌐 Add URL Source", expanded=True):
-        st.text_input("Enter a URL", placeholder="https://example.com", key="rag_url")
+        cols0 = st.columns(2)
+        with cols0[0]:
+            is_vector_db_loaded = ("vector_db" in st.session_state and st.session_state.vector_db is not None)
+            st.toggle(
+                "Use RAG", 
+                value=is_vector_db_loaded, 
+                key="use_rag", 
+                disabled=not is_vector_db_loaded,
+            )
 
-    with st.expander(f"📚 Documents in DB ({0 if not is_vector_db_loaded else len(st.session_state.rag_sources)})"):
-        st.write([] if not is_vector_db_loaded else [source for source in st.session_state.rag_sources])
+        with cols0[1]:
+            st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
 
-with main_content:
-    # Display chat messages inside a scrollable container
-    with st.container():
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        st.header("RAG Sources:")
+            
+        # File upload input for RAG with documents
+        st.file_uploader(
+            "📄 Upload a document", 
+            type=["pdf", "txt", "docx", "md"],
+            accept_multiple_files=True,
+            on_change=load_doc_to_db,
+            key="rag_docs",
+        )
 
-    # User Input with scrollable text box
-    prompt = st.text_area("Your message", height=50)
-    if st.button("Send"):
+        # URL input for RAG with websites
+        st.text_input(
+            "🌐 Introduce a URL", 
+            placeholder="https://example.com",
+            on_change=load_url_to_db,
+            key="rag_url",
+        )
+
+        with st.expander(f"📚 Documents in DB ({0 if not is_vector_db_loaded else len(st.session_state.rag_sources)})"):
+            st.write([] if not is_vector_db_loaded else [source for source in st.session_state.rag_sources])
+
+    
+    # Main chat app
+    model_provider = st.session_state.model.split("/")[0]
+    if model_provider == "openai":
+        llm_stream = ChatOpenAI(
+            api_key=openai_api_key,
+            model_name=st.session_state.model.split("/")[-1],
+            temperature=0.3,
+            streaming=True,
+        )
+    elif model_provider == "anthropic":
+        llm_stream = ChatAnthropic(
+            api_key=anthropic_api_key,
+            model=st.session_state.model.split("/")[-1],
+            temperature=0.3,
+            streaming=True,
+        )
+    elif model_provider == "azure-openai":
+        llm_stream = AzureChatOpenAI(
+            azure_endpoint=os.getenv("AZ_OPENAI_ENDPOINT"),
+            openai_api_version="2024-02-15-preview",
+            model_name=st.session_state.model.split("/")[-1],
+            openai_api_key=os.getenv("AZ_OPENAI_API_KEY"),
+            openai_api_type="azure",
+            temperature=0.3,
+            streaming=True,
+        )
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Your message"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
@@ -108,48 +191,13 @@ with main_content:
             message_placeholder = st.empty()
             full_response = ""
 
-            from langchain.schema import HumanMessage, AIMessage
-            from langchain_openai import ChatOpenAI, AzureChatOpenAI
-            from langchain_anthropic import ChatAnthropic
-            from rag_methods import (
-                load_doc_to_db,
-                load_url_to_db,
-                stream_llm_response,
-                stream_llm_rag_response,
-            )
-
-            messages = [
-                HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"])
-                for m in st.session_state.messages
-            ]
-
-            model_provider = st.session_state.model.split("/")[0]
-            if model_provider == "openai":
-                llm_stream = ChatOpenAI(
-                    api_key=openai_api_key,
-                    model_name=st.session_state.model.split("/")[-1],
-                    temperature=0.3,
-                    streaming=True,
-                )
-            elif model_provider == "anthropic":
-                llm_stream = ChatAnthropic(
-                    api_key=anthropic_api_key,
-                    model=st.session_state.model.split("/")[-1],
-                    temperature=0.3,
-                    streaming=True,
-                )
-            elif model_provider == "azure-openai":
-                llm_stream = AzureChatOpenAI(
-                    azure_endpoint=os.getenv("AZ_OPENAI_ENDPOINT"),
-                    openai_api_version="2024-02-15-preview",
-                    model_name=st.session_state.model.split("/")[-1],
-                    openai_api_key=os.getenv("AZ_OPENAI_API_KEY"),
-                    openai_api_type="azure",
-                    temperature=0.3,
-                    streaming=True,
-                )
+            messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
 
             if not st.session_state.use_rag:
                 st.write_stream(stream_llm_response(llm_stream, messages))
             else:
                 st.write_stream(stream_llm_rag_response(llm_stream, messages))
+
+
+
+    
