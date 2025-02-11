08d086c0c8f9a934615c64a28522868ab2e2cc47
Naga Hema Chand Chinta <42846711+Nagahemachand@users.noreply.github.com>
2025-02-11 00:03:36 -0600

    Added excel file features

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ else:
         # File upload input for RAG with documents
         st.file_uploader(
             "📄 Upload a document", 
-            type=["pdf", "txt", "xlxs", "csv", "docx", "md"],
+            type=["pdf", "txt", "xlsx", "csv", "docx", "md"],
             accept_multiple_files=True,
             on_change=load_doc_to_db,
             key="rag_docs",
